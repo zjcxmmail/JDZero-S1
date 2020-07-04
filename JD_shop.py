1f3da96b49f4cddaa7649a8a398376193c1615b8
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-07-04 21:49:44 +0800

    Create JD_shop.py
    领京豆--进店领豆

---
+++ JD_shop.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import jdCookie
+
+# 领京豆--进店领豆 
+# 每天运行一次  2或4京豆 聊胜于无
+
+
+def getTaskIndex(cookies):
+    headers = {
+        'User-Agent': 'JD4iPhone/167169 (iPhone; iOS 13.4.1; Scale/3.00)',
+        'Host': 'api.m.jd.com',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    params = (
+        ('functionId', 'queryTaskIndex'),
+    )
+
+    data = {
+        'body': '{}',
+        "appid": "ld"
+    }
+
+    response = requests.post('https://api.m.jd.com/client.action', headers=headers,
+                             params=params, cookies=cookies, data=data)
+    result = json.loads(response.text)
+    if "taskList" not in result["data"] or not result["data"]["taskList"]:
+        print("...来晚了")
+        return
+    taskList = result["data"]["taskList"]
+    for i in taskList:
+        taskId = i["taskId"]
+        taskStatus = i["taskStatus"]
+        if taskStatus == 3:
+            print("taskId [", taskId, "]  ok")
+        if taskStatus != 3:
+            print("执行任务")
+            takeTask(cookies, taskId)
+
+
+def takeTask(cookies, taskId):
+    headers = {
+        'User-Agent': 'JD4iPhone/167169 (iPhone; iOS 13.4.1; Scale/3.00)',
+        'Host': 'api.m.jd.com',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    params = (
+        ('functionId', 'takeTask'),
+    )
+
+    data = {
+        'body': f"""{{"taskId":"{taskId}"}}""",
+        "appid": "ld"
+    }
+
+    response = requests.post('https://api.m.jd.com/client.action',
+                             headers=headers, params=params, cookies=cookies, data=data)
+    print(response.text)
+
+
+print("进店领豆  每天运行一次  2或4京豆\n")
+for cookies in jdCookie.get_cookies():
+    print(cookies["pt_pin"])
+    getTaskIndex(cookies)
+    print("\n")

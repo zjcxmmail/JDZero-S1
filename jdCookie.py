e8b3682b6435db2fb57612a01c317219a307aec1
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-07-07 08:35:29 +0800

    Update jdCookie.py

--- jdCookie.py
+++ jdCookie.py
@@ -1,5 +1,5 @@
 import requests
-import re
+import json
 import time
 
 """
@@ -23,38 +23,33 @@ cookies2={}   # 如果有其它账号，还需要将cookies2填写进 下面的c
 cookieLists=[cookies1,]  #多账号准备
 
 
-
-def isNot_valid(cookies):
+def valid(cookies):
     headers = {
         'Host': 'api.m.jd.com',
         'Accept': '*/*',
         'Connection': 'keep-alive',
-        'User-Agent': 'jdapp;iPhone;8.5.5;13.4;9b812b59e055cd226fd60ebb5fd0981c4d0d235d;network/wifi;supportApplePay/3;hasUPPay/0;pushNoticeIsOpen/0;model/iPhone9,2;addressid/138109592;hasOCPay/0;appBuild/167121;supportBestPay/0;jdSupportDarkMode/0;pv/103.2;apprpd/MyJD_Main;ref/https%3A%2F%2Fbean.m.jd.com%2FplantBean%2Findex.action%3Fsource%3Dwojing%26un_area%3D5_274_49707_49973%26lng%3D116.8438383685941%26lat%3D39.95744163210918;psq/3;ads/;psn/9b812b59e055cd226fd60ebb5fd0981c4d0d235d|219;jdv/0|direct|-|none|-|1583449735697|1583796810;adk/;app_device/IOS;pap/JA2015_311210|8.5.5|IOS 13.4;Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
+        'User-Agent': 'jdapp;iPhone;8.5.5;13.5;Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
         'Accept-Language': 'zh-cn',
         'Accept-Encoding': 'gzip, deflate, br',
     }
     params = (
         ('functionId', 'plantBeanIndex'),
-        ('body', f"""{{"monitor_refer":"","wxHeadImgUrl":"","shareUuid":"7pt22jcko7ljrbpeask7r6avre3h7wlwy7o5jii","followType":"1","monitor_source":"plant_m_plant_index","version":"8.4.0.0"}}"""),
+        ('body', json.dumps(
+            {"monitor_source": "plant_m_plant_index", "version": "8.4.0.0"})),
         ('appid', 'ld'),
-        ('client', 'apple'),
-        ('clientVersion', '8.5.5'),
-        ('networkType', 'wifi'),
-        ('osVersion', '13.4'),
-        ('uuid', '9b812b59e055cd226fd60ebb5fd0981c4d0d235d'),
-        ('jsonp', 'jsonp_1585181731771_80279'),
     )
     response = requests.get('https://api.m.jd.com/client.action',
                             headers=headers, params=params, cookies=cookies)
-    f= re.fullmatch(r"jsonp\w+\({\"code\":\"3\"}\);", response.text,flags=0) #返回None表示有效；反之
-    if f!=None:
+    if response.json()["code"] == "3":
         print(f"""## {cookies["pt_pin"]}: cookie过期""")
-    return f
+        return False
+    return True
 
 def get_cookies():
-    i= [i for i in cookieLists if not isNot_valid(i)]
-    return i
-    
+    return [i for i in cookiesLists if valid(i)]
+
+
+# get_cookies()
 print("***"*20)
 print("***"*20)
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))

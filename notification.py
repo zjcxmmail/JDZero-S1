08662306afaec2d3de152c46c252735a1bb28797
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-08-20 08:39:10 +0800

    Update notification.py
    fix bug

--- notification.py
+++ notification.py
@@ -2,21 +2,22 @@ import requests
 import os
 import smtplib
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from email.header import Header
+import time
 
 
 #######################
 # 通知服务
 #######################
 
-needYou2Know = 0    # [0,1,2]  0:不通知     1:server酱      2:SMTP邮件服务
+needYou2Know = 0   # [0,1,2]  0:不通知     1:server酱      2:SMTP邮件服务
 
 SCKEY = ''        # Server酱的SCKEY
 email_dict = {
     "sender": '',                 # ① sender是邮件发送人邮箱
     "passWord": '',               # ② passWord是服务器授权码
     "mail_host": 'smtp.qq.com',   # ③ mail_host是服务器地址（这里是QQsmtp服务器）
-    "port": "465",                # ④ QQsmtp服务器的端口号为465或587
+    "port": 465,                  # ④ QQsmtp服务器的端口号为465或587
     "receivers": ['']             # ⑤ receivers是邮件接收人，用列表保存，可以添加多个
 }
 ##################################################################
@@ -34,23 +35,25 @@ def send_email(subject, msg_content):
         print("SMTP服务的未设置!!\n取消推送")
         return
     print("SMTP服务启动")
-    msg = MIMEMultipart()
-    msg['Subject'] = subject
-    msg['From'] = email_dict["sender"]
-    msg.attach(MIMEText(msg_content, 'text', 'utf-8'))
+    mail_host = email_dict["mail_host"]
+    mail_user = email_dict["sender"]
+    mail_pass = email_dict["passWord"]
+    sender = mail_user
+    receivers = email_dict["receivers"]
+    message = MIMEText(msg_content, 'plain', 'utf-8')
+    message['From'] = Header("JD_tools Scripts", 'utf-8')
+    message['To'] = Header("User", 'utf-8')
+    message['Subject'] = Header(subject, 'utf-8')
     try:
-        s = smtplib.SMTP_SSL(email_dict["mail_host"], email_dict["port"])
-        s.set_debuglevel(0)
-        s.login(email_dict["sender"], email_dict["passWord"])
-        # 给receivers列表中的联系人逐个发送邮件
-        for item in email_dict["receivers"]:
-            msg['To'] = to = item
-            s.sendmail(email_dict["sender"], to, msg.as_string())
-            print('Success!')
-        s.quit()
-        print("All emails have been sent over!")
+        smtpObj = smtplib.SMTP_SSL(mail_host, email_dict["port"])  # ssl加密
+        # smtpObj = smtplib.SMTP(mail_host, 25)                    # 明文发送
+        smtpObj.login(mail_user, mail_pass)
+        print("邮件登录成功")
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.quit()
+        print("邮件发送成功")
     except smtplib.SMTPException as e:
-        print("Falied,%s", e)
+        print("Error: 无法发送邮件", e)
 
 
 def serverJ(title, content):
@@ -78,4 +81,6 @@ notify = [n0, serverJ, send_email][needYou2Know]
 
 if __name__ == "__main__":
     print("通知服务测试")
-    notify("JD_tool脚本通知", "needYou2Know")
+    start = time.time()
+    notify("JD_tools脚本通知服务", "needYou2Know\n通知服务测试")
+    print("耗时: ", time.time()-start, "s")

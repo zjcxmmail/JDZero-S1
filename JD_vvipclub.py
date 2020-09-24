114c3e99eaccc3aaeeaf2278541323f467ecafc9
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-09-24 20:15:22 +0800

    Create JD_vvipclub.py
    入口: app首页"领京东"——"摇京豆"
    做任务得次数
    好久没写了, 新写了一个

---
+++ JD_vvipclub.py
@@ -0,0 +1,71 @@
+import requests
+import json
+import jdCookie
+import time
+
+"""
+入口: app首页 "领京东" ——  "摇京豆"
+cron 5,8 2 * * *
+"""
+
+def template(cookies, functionId, body):
+    headers = {
+        'User-Agent': 'jdapp;iPhone;9.0.2;13.5.1',
+        'Host': 'api.m.jd.com',
+        'Referer': 'https://vip.m.jd.com/newPage/reward/123dd/slideContent?page=focus',
+    }
+
+    params = (
+        ('appid', 'vip_h5'),
+        ('functionId', functionId),
+        ('body', json.dumps(body)),
+        ('_', str(int(time.time()*1000))),
+    )
+
+    response = requests.get('https://api.m.jd.com/client.action',
+                            headers=headers, params=params, cookies=cookies)
+    return response.json()
+
+
+def shake(cookies):
+    result = template(cookies, "vvipclub_luckyBox", {
+        "info": "freeTimes"})
+    return result["data"]["freeTimes"]
+
+
+for cookies in jdCookie.get_cookies():
+    print(cookies["pt_pin"])
+    browseTask = template(cookies, "vvipclub_lotteryTask", {
+        "info": "browseTask", "withItem": True})["data"][0]
+    time.sleep(1)
+    attentionTask = template(cookies, "vvipclub_lotteryTask", {
+        "info": "attentionTask", "withItem": True})["data"][0]
+    # print(attentionTask)
+    # exit()
+    m = browseTask["totalPrizeTimes"]-browseTask["currentFinishTimes"]
+    print("browseTask: ",m)
+    if m > 0:
+        _ids = [i["id"] for i in browseTask["taskItems"] if not i["finish"]]
+        for i in range(m):
+            print(template(cookies, "vvipclub_doTask", {
+                "taskName": "browseTask", "taskItemId": _ids.pop()}))
+            time.sleep(1)
+
+    n = attentionTask["totalPrizeTimes"]-attentionTask["currentFinishTimes"]
+    time.sleep(1)
+    print("attentionTask: ",n)
+    if n > 0:
+        _ids = [i["id"] for i in attentionTask["taskItems"] if not i["finish"]]
+        for i in range(n):
+            print(template(cookies, "vvipclub_doTask", {
+                "taskName": "attentionTask", "taskItemId": str(_ids.pop())}))
+            time.sleep(2)
+    freeTimes = shake(cookies)
+    print("freeTimes",freeTimes)
+    for i in range(freeTimes):
+        print(template(cookies, "vvipclub_shaking", {
+            "type": "0"}))
+        time.sleep(1)
+    time.sleep(1)
+    print("\n\n")
+    print("##"*30)

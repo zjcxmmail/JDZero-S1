80cd6a39e99db8e4f44747ecafdafd5f1ecf36e0
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-09-24 20:28:02 +0800

    Update notification.py
    新增bark通知服务,bark需要下载app,需要的自行搜索

--- notification.py
+++ notification.py
@@ -9,9 +9,10 @@ import time
 # 通知服务
 #######################
 
-needYou2Know = 0   # [0,1,2]  0:不通知     1:server酱      2:SMTP邮件服务
+needYou2Know = 0   # [0,1,2,3]  0:不通知     1:server酱      2:SMTP邮件服务        3:bark服务
 
 SCKEY = ''        # Server酱的SCKEY
+
 email_dict = {
     "sender": '',                 # ① sender是邮件发送人邮箱
     "passWord": '',               # ② passWord是服务器授权码
@@ -19,6 +20,8 @@ email_dict = {
     "port": 465,                  # ④ QQsmtp服务器的端口号为465或587
     "receiver": ''                # ⑤ receiver是邮件接收人
 }
+
+BARK = ''         # bark服务,自行搜索
 ##################################################################
 
 
@@ -75,8 +78,23 @@ def serverJ(title, content):
     response = requests.post(f"https://sc.ftqq.com/{sckey}.send", data=data)
     print(response.text)
 
-
-notify = [n0, serverJ, send_email][needYou2Know]
+def bark(title, content):
+    """bark服务"""
+    bark_token = BARK
+    if "BARK" in os.environ:
+        """
+        判断是否运行自GitHub action,"BARK" 该参数与 repo里的Secrets的名称保持一致
+        """
+        bark_token = os.environ["BARK"]
+    if not bark_token:
+        print("bark服务的bark_token未设置!!\n取消推送")
+        return
+    response = requests.get(
+        f"""https://api.day.app/{bark_token}/{title}/{content}""")
+    print(response.text)
+    
+    
+notify = [n0, serverJ, send_email, bark][needYou2Know]
 
 if __name__ == "__main__":
     print("通知服务测试")

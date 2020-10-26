0b6394f90c31325fe60c0804273d67300aa66641
Lance <23739314+Zero-S1@users.noreply.github.com>
2020-10-26 09:47:46 +0800

    Update jd_farm_help.py
    fix bug

--- jd_farm_help.py
+++ jd_farm_help.py
@@ -56,6 +56,7 @@ def help(cookies):
     print("\n")
     data = postTemplate(cookies, "friendListInitForFarm",
                         {})
+
     print("他人运行此脚本为我助力")
     print(f"""今日新增好友: {data["inviteFriendCount"]}/10""")
     if data["inviteFriendCount"] > 0 and data["inviteFriendCount"] > data["inviteFriendGotAwardCount"]:
@@ -65,18 +66,22 @@ def help(cookies):
     myFriendCode = [i["shareCode"]
                     for i in data["friends"] if "shareCode" in i]
     countOfFriend = data["countOfFriend"]
-    #lastId = [i for i in data["friends"]][-1]["id"]
-    _friendsList=[i for i in data["friends"]]
+    _friendsList = [i for i in data["friends"]]
     if not _friendsList:
         print("好友列表为空  跳出")
         print(data)
         return
-    lastId =  _friendsList[-1]["id"]
+    lastId = _friendsList[-1]["id"]
     print(f"""fullFriend:{data["fullFriend"]}""")  # 好友添加总数有上限
+    if data["fullFriend"]:
+        print("好友达到上限,退出")
+        return
     for i in range(countOfFriend//20):
         result = postTemplate(cookies, "friendListInitForFarm",
                               {"lastId": lastId})
         pageFriend = [i["shareCode"] for i in result["friends"]]
+        if not result["friends"]:
+            break
 
         lastId = [i for i in result["friends"]][-1]["id"]
         myFriendCode += pageFriend
@@ -87,7 +92,7 @@ def help(cookies):
         set(shareCodes).difference(myFriendCode, [myshareCode]))  # 去掉自己以及已是好友关系的shareCode
     print("准备助力的shareCodes:", shareCodes_diff)
     if not shareCodes_diff:
-        print("脚本中的shareCodes暂时没发现新好友")
+        print("脚本中的shareCodes暂时没发现新好友,退出助力")
     for i in shareCodes_diff:
         data = postTemplate(cookies, "initForFarm", {
             "shareCode": f"{i}-inviteFriend"})

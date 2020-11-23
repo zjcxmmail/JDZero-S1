b862fa61ee09115360cfd83b0deb9845f89e537c
Lance <23739314+haiBosuPal@users.noreply.github.com>
2020-11-23 11:43:18 +0800

    fix bug

--- notification.py
+++ notification.py
@@ -9,7 +9,8 @@ import time
 # 通知服务
 #######################
 
-needYou2Know = 0   # [0,1,2,3]  0:不通知     1:server酱      2:SMTP邮件服务        3:bark服务
+# [0,1,2,3]  0:不通知     1:server酱      2:SMTP邮件服务        3:bark服务
+needYou2Know = 0
 
 SCKEY = ''        # Server酱的SCKEY
 
@@ -78,9 +79,12 @@ def serverJ(title, content):
     response = requests.post(f"https://sc.ftqq.com/{sckey}.send", data=data)
     print(response.text)
 
+
 def bark(title, content):
     """bark服务"""
     bark_token = BARK
+    title = content.replace("#", "")
+    content = content.replace("#", "")
     if "BARK" in os.environ:
         """
         判断是否运行自GitHub action,"BARK" 该参数与 repo里的Secrets的名称保持一致
@@ -92,8 +96,8 @@ def bark(title, content):
     response = requests.get(
         f"""https://api.day.app/{bark_token}/{title}/{content}""")
     print(response.text)
-    
-    
+
+
 notify = [n0, serverJ, send_email, bark][needYou2Know]
 
 if __name__ == "__main__":
